e4ef1c59a39afbb182ede4c759c201ca1a488967
kamja-kim <pkknq@naver.com>
2025-06-29 13:19:32 +0900

    bootstrap
    (cherry picked from commit 0fbd4d66485b4ea8f812e99c5fc492c9094c0653)

---
+++ src/main/kotlin/com/side/highlights/HighlightsApplication.kt
@@ -0,0 +1,11 @@
+package com.side.highlights
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HighlightsApplication
+
+fun main(args: Array<String>) {
+    runApplication<HighlightsApplication>(*args)
+}
---
+++ src/test/kotlin/com/side/highlights/HighlightsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.side.highlights
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HighlightsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
